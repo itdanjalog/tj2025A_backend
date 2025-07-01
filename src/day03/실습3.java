8464e9c404ea5315348f4f26545e83dab6c53f53
itdanjalog <kgs2072@naver.com>
2025-07-01 13:11:35 +0900

    day03

--- src/day03/실습3.java
+++ src/day03/실습3.java
@@ -59,30 +59,82 @@ public class 실습3 { // class s
         */
 
         // 문제7 : 최동진
+        /*
         System.out.print("정수를 입력하세요 : ");     int i1 = scan.nextInt(); // 1. 하나의 정수를 입력받아 저장
         System.out.print("결과:");
         System.out.println(i1 % 2 == 1 && i1 % 7 == 0 ); // 2. 정수가 홀수 이면서 정수가 7의 배수, &&이면서
+        */
 
         // 문제8 : 민성호 : 삼항연산자 목적 : true 와 false 대신에 다른 자료로 변경
+        /*
         System.out.print("1차점수 : ");            int score1 = scan.nextInt();
         System.out.print("2차 점수 : ");           int score2 = scan.nextInt();
         System.out.println( score1+score2 >= 150 ? "합격" : "불합격");
+        */
 
         // 문제9 : 신연우 : 삼항연산자 , 연결연산자 , .문자열비교
+        /*
         System.out.print("이름을 입력하세요 : ");   String nameInput1 = scan.next();    // 1. 하나의 String(문자열) 입력받아 저장
         System.out.println( nameInput1.equals("유재석") ? nameInput1 + "(방장)" : nameInput1 );
                             // 삼항연산자 : 조건 ? 참 : 거짓
                             // 조건           : 만약에 입력값(nameInput1) 이 "유재석" 이면
                             // 참(true)       : 입력값(nameInput1) 뒤에 "(방장)" 문자열 +연결
                             // 거짓(false)    :  입력값(nameInput1)
+        */
+
         // 문제10 : 김진숙
+        /*
+        System.out.print( "삼각형 밑변 : " );            double triBase = scan.nextDouble();
+        System.out.print( "삼각형 높이 : " );            double triHeight = scan.nextDouble();
+        double triextent = (triBase * triHeight) / 2.0; // [자동타입변환]
+        System.out.printf( "삼각형의 넓이: %.1f \n" , triextent );
+        */
+
         // 문제11 : 김재영
+        /*
+        System.out.print("섭씨 온도: ");                double cDegree = scan.nextDouble();
+        double fDegree = (cDegree * 9.0 / 5.0) + 32;    // 9(int) vs 9.0(double)
+        System.out.println("화씨 온도: " + fDegree);
+        */
+
         // 문제12 : 박옥선
+        /*
+        System.out.print("태어난 년도를 입력하세요 : "); int birthYear = scan.nextInt();
+        int age = 2025 - birthYear;
+        System.out.println("2025년 기준 나이 : " + age );
+        */
+
         // 문제13 : 신승민
-        // 문제14 : 옹태경
+        System.out.print("키(cm): ");            double height = scan.nextDouble();
+        System.out.print("몸무게(kg): ");        double weight = scan.nextDouble();
+        double meter = height / 100.0;          // cm --> m 변경 , 100(int) vs 100.0(double)
+        double BMI = weight / (meter * meter);  // BMI 계산식
+        System.out.printf("BMI 지수: %.2f\n", BMI);
+        // 문제14 : 옹태경 , 문자열비교 : "문자열A".equals("문자열B")
+        System.out.print("아이디: ");              String id14 = scan.next();
+        System.out.print("이메일: ");              String email14 = scan.next();
+        String result14 = (id14.equals("admin")) || (email14.equals("admin@test.com")) ? "관리자" : "일반 사용자" ;
+        System.out.println("결과: " + result14);
         // 문제15 : 송지훈
-        // 문제16 : 이찬영
+        System.out.print("점수 입력 : ");           int 점수 = scan.nextInt();
+        String 등급 = (점수 >= 90) ? "A" : (점수 >= 80 ) ? "B" : (점수 >= 70) ? "C" : "F" ;
+            // (삼항연산자 중첩) 조건1 ? 참1 : 조건2 ? 참2 : 조건3 ? 참3 : 거짓(그외)   ---> 복잡도 증가 ---> if사용
+        System.out.printf("등급 : %s \n" , 등급);
+        // 문제16 : 이찬영 , 20<=age<=29 [x] 범위논리없다 , &&이면서 활용
+        System.out.print("나이 입력");            int age = scan.nextInt();
+        String event = age >= 20 && age<= 29 ? "이벤트 대상입니다" : "이벤트 대상이 아닙니다";
+        System.out.println(event);
 
     } // main e
 } // class e
+
+
+
+
+
+
+
+
+
+
 
