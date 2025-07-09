c997f0400b4767057e74f73aff567f7078874aba
itdanjalog <kgs2072@naver.com>
2025-07-09 14:55:02 +0900

    day08

--- src/종합/예제5/BoardService5.java
+++ src/종합/예제5/BoardService5.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class BoardService5 {
     public static void main(String[] args) {
 
+        // * BoardController 클래스내 *메소드를 사용*하기 위한 객체 생성
+        BoardController bs = new BoardController();
+
         for( ; ; ) {// (1) 무한루프 : 프로그램 계속적으로 실행하기 위해서
             System.out.println("============= My Community =============");// (2) 출력
             System.out.println("  1.게시물쓰기 | 2.게시물출력");
@@ -14,13 +17,23 @@ public class BoardService5 {
             int choose = scan.nextInt();
             if( choose == 1 ) { // (4) 입력 에 따른 화면 출력
                 System.out.print("내용 : ");
+                String content = scan.next();
                 System.out.print("작성자 : ");
-                System.out.println("[안내] 글쓰기 성공");
+                String writer = scan.next();
+                boolean result = bs.doPost( content , writer );
+                if( result ){  System.out.println("[안내] 글쓰기 성공");}
+                else{ System.out.println("[경고] 글쓰기 실패");  }
             }else if( choose == 2 ) {
                 System.out.println("============= 게시물 목록 =============");
-                System.out.println("작성자 : " );
-                System.out.println("내용 : " );
-                System.out.println("------------------------------------");
+                Board[] boards = bs.doGet();
+                for( int index = 0 ; index <= boards.length - 1 ; index++ ){
+                    Board board = boards[index];
+                    if( board != null ){
+                        System.out.println("작성자 : " + board.writer );
+                        System.out.println("내용 : " + board.content );
+                        System.out.println("------------------------------------");
+                    }
+                }
             } // if end
         } // for end
 
