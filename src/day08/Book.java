69a628a1bd7fbdf97a8b7c0f47d0f59a7de9caf5
itdanjalog <kgs2072@naver.com>
2025-07-09 12:53:11 +0900

    day08

---
+++ src/day08/Book.java
@@ -0,0 +1,17 @@
+package day08;
+
+public class Book {
+    // 멤버변수
+    String title;   // 이름
+    String author;  // 저자
+    int price;   // 가격
+
+    // 생성자 : title, author, price를 매개변수로 받아
+    Book( String title , String author , int price ){
+        // 멤버 변수를 초기화하는
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    } // end
+
+} // class end
